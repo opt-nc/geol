9a63f1aed93d3cc72f18872d417ec173a2fddb3d
Vinh Faucher <vinh.faucher@opt.nc>
2025-10-14 15:46:18 +1100

    fix: linter errors

--- cmd/tag.go
+++ cmd/tag.go
@@ -40,33 +40,37 @@ geol tag canonical`,
 		tagsPath, err := utilities.GetTagsPath()
 		if err != nil {
 			fmt.Println("Error retrieving tags path:", err)
-			return
+			os.Exit(1)
 		}
 		tags, err := utilities.GetTagsWithCacheRefresh(cmd, tagsPath)
 		if err != nil {
 			fmt.Println("Error retrieving tags from cache:", err)
-			return
+			os.Exit(1)
 		}
 		if _, ok := tags[tag]; !ok {
 			fmt.Printf("Tag '%s' not found in cache.\n", tag)
-			return
+			os.Exit(1)
 		}
 
 		url := utilities.ApiUrl + "tags/" + tag
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Printf("Error requesting tag '%s': %v\n", tag, err)
-			return
+			os.Exit(1)
 		}
 		body, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
+		closeErr := resp.Body.Close()
 		if err != nil {
 			fmt.Printf("Error reading response for tag '%s': %v\n", tag, err)
-			return
+			os.Exit(1)
+		}
+		if closeErr != nil {
+			fmt.Printf("Error closing response body for tag '%s': %v\n", tag, closeErr)
+			os.Exit(1)
 		}
 		if resp.StatusCode != 200 {
 			fmt.Printf("Tag '%s' not found on the API.\n", tag)
-			return
+			os.Exit(1)
 		}
 
 		var apiResp struct {
@@ -81,7 +85,7 @@ geol tag canonical`,
 		}
 		if err := json.Unmarshal(body, &apiResp); err != nil {
 			fmt.Printf("Error decoding JSON for tag '%s': %v\n", tag, err)
-			return
+			os.Exit(1)
 		}
 
 		tagColor := color.New(color.Bold)
@@ -92,10 +96,16 @@ geol tag canonical`,
 			tagNode.Child(productColor.Sprint(prod.Name))
 		}
 		treeRoot.Child(tagNode)
-		fmt.Fprintln(os.Stdout, treeRoot.String())
+		if _, err := fmt.Fprintln(os.Stdout, treeRoot.String()); err != nil {
+			fmt.Printf("Error printing tree for tag '%s': %v\n", tag, err)
+			os.Exit(1)
+		}
 		nbProducts := len(apiResp.Result)
 		nbProductsStyle := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("2"))
-		fmt.Fprintf(os.Stdout, "\n%s products listed for tag '%s'\n", nbProductsStyle.Render(fmt.Sprintf("%d", nbProducts)), tag)
+		if _, err := fmt.Fprintf(os.Stdout, "\n%s products listed for tag '%s'\n", nbProductsStyle.Render(fmt.Sprintf("%d", nbProducts)), tag); err != nil {
+			fmt.Printf("Error printing product count for tag '%s': %v\n", tag, err)
+			os.Exit(1)
+		}
 	},
 }
 
