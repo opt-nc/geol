4ed82b93f9811f3aae174944a8240ff776e3da93
Vinh Faucher <vinh.faucher@opt.nc>
2025-12-16 09:26:10 +1100

    fix(#96): add flags output and force

--- cmd/exports/duckdb.go
+++ cmd/exports/duckdb.go
@@ -3,9 +3,10 @@ package exports
 import (
 	"database/sql"
 	"fmt"
-	"log"
+	"os"
 
 	_ "github.com/duckdb/duckdb-go/v2"
+	"github.com/phuslu/log"
 	"github.com/spf13/cobra"
 
 	"github.com/opt-nc/geol/utilities"
@@ -22,14 +23,25 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		dbPath, _ := cmd.Flags().GetString("output")
+		forceDuckDB, _ := cmd.Flags().GetBool("force")
+		if _, err := os.Stat(dbPath); err == nil {
+			if !forceDuckDB {
+				log.Fatal().Msgf("File %s already exists. Use --force to overwrite.", dbPath)
+			}
+			if err := os.Remove(dbPath); err != nil {
+				log.Fatal().Err(err).Msgf("Error removing existing %s file", dbPath)
+			}
+		}
+
 		// Open or create geol.duckdb at project root
-		db, err := sql.Open("duckdb", "geol.duckdb")
+		db, err := sql.Open("duckdb", dbPath)
 		if err != nil {
-			log.Fatalf("Error while creating DuckDB database: %v", err)
+			log.Fatal().Err(err).Msg("Error while creating DuckDB database")
 		}
 		defer func() {
 			if err := db.Close(); err != nil {
-				log.Fatalf("Error closing DuckDB database: %v", err)
+				log.Fatal().Err(err).Msg("Error closing DuckDB database")
 			}
 		}()
 
@@ -40,11 +52,11 @@ to quickly create a Cobra application.`,
 				goVersion TEXT,
 				platform TEXT,
 				githubURL TEXT,
-				generatedAt TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')::TIMESTAMP,
-				generatedAtTZ TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+				generatedAt TIMESTAMP DEFAULT date_trunc('second', CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+				generatedAtTZ TIMESTAMPTZ DEFAULT date_trunc('second', CURRENT_TIMESTAMP)
 			)`)
 		if err != nil {
-			log.Fatalf("Error creating 'about' table: %v", err)
+			log.Fatal().Err(err).Msg("Error creating 'about' table")
 		}
 
 		// Insert values into 'about' table
@@ -54,14 +66,15 @@ to quickly create a Cobra application.`,
 			fmt.Sprintf("%s/%s", utilities.PlatformOs, utilities.PlatformArch),
 			"https://github.com/opt-nc/geol")
 		if err != nil {
-			log.Fatalf("Error inserting into 'about' table: %v", err)
+			log.Fatal().Err(err).Msg("Error inserting into 'about' table")
 		}
 
-		fmt.Println("Table 'about' created and metadata inserted into geol.duckdb.")
+		log.Info().Msg("DuckDB database created successfully at " + dbPath)
 	},
 }
 
 func init() {
 	ExportCmd.AddCommand(duckdbCmd)
-
+	duckdbCmd.Flags().StringP("output", "o", "geol.duckdb", "Output DuckDB database file path")
+	duckdbCmd.Flags().BoolP("force", "f", false, "Force overwrite of existing geol.duckdb file")
 }
